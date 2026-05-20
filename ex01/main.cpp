c9a5c78ea43968a53539c9a7efd3245e6d8553ea
C4r4cole <c4r4cole.chevalerie@gmail.com>
2026-05-20 16:40:44 +0200

    ex01 init

---
+++ ex01/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fmoulin <fmoulin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/31 14:32:59 by fmoulin           #+#    #+#             */
+/*   Updated: 2026/05/20 16:39:46 by fmoulin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "BitcoinExchange.hpp"
+
+int main(int argc, char **argv)
+{
+
+	return 0;
+}
+
+// Avec map
+	// tri automatique
+	// recherche logarithmique
+	// recherche par clé
+	// bornes (lower_bound)
+	// unicité des clés
